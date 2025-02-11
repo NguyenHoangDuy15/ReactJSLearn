f843236669bed194c461be799316c81d22d44eef
NguyenHoangDuy15 <nguyenhoangduy004bn@gmail.com>
2025-02-11 18:49:47 +0700

    Bai 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,21 @@
+// class component
+// functional component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+  // JSX
+  render() {
+    return (
+      <div>
+        {/* JSX */
+        Math.PI
+        }
+
+        <h1>Hello World from MyComponent</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
