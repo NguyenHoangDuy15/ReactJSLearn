474213851d19214a75eb535df32145d35f3de942
NguyenHoangDuy15 <nguyenhoangduy004bn@gmail.com>
2025-02-11 19:26:54 +0700

    dom event

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import React from "react";
-import MyComponent from "./components/MyComponent";
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import React from 'react';
+import MyComponent from './components/MyComponent';
+const App = () => {
+  const count = useSelector(state => state.counter.count);
+  const dispatch = useDispatch();
 
-class App extends React.Component {
-  state = {
-    name: "React Redux",
-    address: "Hanoi",
-    age: 20,
-  };
+  return (
 
-  render() {
-    return (
-      <div>
-        My name is {this.state.name} and I am {this.state.age} years old. I am living in {this.state.address}
-      </div>
-    );
-  }
+    <div>
+      Hello World from React Redux
+      <MyComponent></MyComponent>
+    </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hello World from React Redux
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
+  );
 }
-
-// --> tu function component thanh class component
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello World from React Redux
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,15 +4,28 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "React Redux",
+    address: "Hanoi",
+    age: 20,
+  };
+
+  handleClick(event) {
+    console.log("Button is clicked");
+    console.log("My name is", this.state.name);
+  }
+  handleOnMouseOver(event) {
+    console.log("Mouse is over");
+    console.log(event.pageX);
+  }
   // JSX
   render() {
     return (
       <div>
-        {/* JSX */
-        Math.PI
-        }
-
-        <h1>Hello World from MyComponent</h1>
+        My name is {this.state.name} and I am {this.state.age} years old. I am
+        living in {this.state.address}
+        <button onClick={this.handleClick}>Click me</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
       </div>
     );
   }
