8e8125a7eb44e6cb3df43163fae27ce30cf2b197
NguyenHoangDuy15 <nguyenhoangduy004bn@gmail.com>
2025-02-19 18:04:08 +0700

    video 49

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
-import './App.scss';
+import "./App.scss";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  
   return (
+    <div className="app-container">
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-content">
+          <Outlet />
+        </div>
+      </div>
 
-    <div className='app-container'>
-      Hello World from React Redux
     </div>
-
   );
-}
+};
 
 export default App;
